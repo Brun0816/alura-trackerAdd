0f8eb784b507d646b3bba689651b09d3c254c0ac
Bruno <brunocarreira816@gmail.com>
2024-10-23 07:41:04 -0300

    Proj Alura-Tracker 23/10/24

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -17,10 +17,10 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useStore } from '@/store';
-import { ALTERA_PROJETO, ADICIONA_PROJETO } from '@/store/tipo-mutacoes';
 import { TipoNotificacao } from '@/interfaces/INotificacao';
 
 import useNotificador from '@/hooks/notificador'
+import { ALTERAR_PROJETO, CADASTRAR_PROJETOS } from '@/store/tipo-acoes';
 
 export default defineComponent({
     name: 'FormularioWeb',
@@ -43,14 +43,16 @@ export default defineComponent({
     methods: {
         salvar() {
             if (this.id) {
-                this.store.commit(ALTERA_PROJETO, {
+                this.store.dispatch(ALTERAR_PROJETO, {
                     id: this.id,
                     nome: this.nomeDoProjeto
-                })
+                }).then(() => this.lidarComSucesso());
             } else {
-                this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
+                this.store.dispatch(CADASTRAR_PROJETOS, this.nomeDoProjeto)
+                    .then(() => this.lidarComSucesso());
             }
-
+        },
+        lidarComSucesso() {
             this.nomeDoProjeto = '';
             this.notificar(TipoNotificacao.SUCESSO, 'Excelente!', 'O projeto foi cadastrado com sucesso!')
             this.$router.push('/projetos')
